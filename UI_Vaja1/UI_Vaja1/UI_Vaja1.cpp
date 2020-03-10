81843c6afecfadba5775387c2cb21d8cb55ca179
Rok Rozman <rok.rozman2@student.um.si>
2020-03-10 19:54:12 +0100

    Naredil VS projekt

---
+++ UI_Vaja1/UI_Vaja1/UI_Vaja1.cpp
@@ -0,0 +1,18 @@
+/**
+    TO DO:
+        Sahovnica more bit velikosti med 4 in 12, toliko je tudi kraljic, vsaka v svojem stolpcu
+        Ustvarit array ki bo imel koordinate kraljic (index je x, vrednost y)
+        hill climbing
+        local beam search
+        simulated annealing
+        genetic algorithm
+        
+        POGLEJ DODATNE PARAMETRE!
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
